d752c597519800f6e5e9b659b926b999304f0deb
jovanatomasevic <66180337+jovanatomasevic@users.noreply.github.com>
2020-12-22 13:09:29 +0100

    Add files via upload
    Додато је дугме за крај игре, као и за поновно покретање и тајмер чије функционалности је потребно још дорадити.

--- hw-3/kviz.js
+++ hw-3/kviz.js
@@ -31,11 +31,11 @@
     ponudjeniOdgovori: ["а)V","б)A", "в)W"],
     tacanOdgovor: 2
   }, {
-    pitanje: "Ако је дато \,,и\"(and) коло, а на улаз доводимо вредности 0 и 1, која вредност ће бити на излазу?",
+    pitanje: "Ако је дато ,,и\"(and) коло, а на улаз доводимо вредности 0 и 1, која вредност ће бити на излазу?",
     ponudjeniOdgovori: ["a)1","б)0"],
     tacanOdgovor: 1
   },{
-    pitanje: "Ко је написао дело ,,\Сумњиво лице\"?",
+    pitanje: "Ко је написао дело ,,Сумњиво лице\"?",
     ponudjeniOdgovori:  ["a)Стеван Сремац","б)Коста Трифковић", "в)Бранислав Нушић"],
     tacanOdgovor: 2
   }, {
@@ -46,17 +46,32 @@
   
   var brojacPitanja = 0;
   var izabranOdg = [];
-  var quizSpace = $('#quiz');
+  var prostorKviza = $('#kviz');
     
   narednoPitanje();
-    
   $('#naredno').click(function () 
     {
           chooseOption();
           brojacPitanja++;
           narednoPitanje();
+          myTimer();
         
     });  
+   
+   $('#kraj').click(function krajIgre()
+   {
+        var scoreRslt = displayResult();
+        prostorKviza.append(scoreRslt).fadeIn();
+        $('#pitanje').remove();
+        $('#naredno').hide();
+        $('#kraj').hide();
+        myStopFunction();
+        $(document).ready(function() {
+       $('#container').append('<button class="btn-styled" type="button"><a href="kviz.html"><span>Покрени поново</span></a></button>');
+});
+      });
+  
+   
   function createElement(index) 
     {
         var element = $('<div>',{id: 'pitanje'});
@@ -68,7 +83,6 @@
 
         var radio = radioButtons(index);
         element.append(radio);
-
         return element;
     }
   
@@ -94,13 +108,14 @@
    
   function narednoPitanje() 
     {
-        quizSpace.fadeOut(function() 
+      prostorKviza.fadeOut(function() 
             {
               $('#pitanje').remove();
+              setInterval(myVar);
               if(brojacPitanja < svaPitanja.length)
                 {
                     var narednoPitanje = createElement(brojacPitanja);
-                    quizSpace.append(narednoPitanje).fadeIn();
+                    prostorKviza.append(narednoPitanje).fadeIn();
                     if (!(isNaN(izabranOdg[brojacPitanja]))) 
                     {
                       $('input[value='+izabranOdg[brojacPitanja]+']').prop('checked', true);
@@ -113,12 +128,12 @@
               else 
                 {
                     var scoreRslt = displayResult();
-                    quizSpace.append(scoreRslt).fadeIn();
+                    prostorKviza.append(scoreRslt).fadeIn();
                     $('#naredno').hide();
+                  
                 }
         });
     }
-  
   function displayResult() 
     {
         var score = $('<p>',{id: 'pitanje'});
@@ -133,4 +148,26 @@
         score.append('Укупно ' + correct + ' од ' +svaPitanja.length);
         return score;
   }
-})();
+}) ();
+
+var myVar = setInterval(function(){ myTimer() }, 1000);
+var vreme = 20;
+function myTimer() {
+if(vreme == 0)
+{
+    myStopFunction();
+}
+document.getElementById("prikazTajmera").innerHTML = '00:' + zeroPad(vreme,2);
+vreme = vreme  - 1;
+
+}
+
+function myStopFunction() {
+  document.getElementById("prikazTajmera").innerHTML = '00:' + zeroPad(0,2);
+  clearInterval(myVar);
+}
+function zeroPad(num, places) {
+  var zero = places - num.toString().length + 1;
+  return Array(+(zero > 0 && zero)).join("0") + num;
+}
+
